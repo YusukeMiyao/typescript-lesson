ffa1ab191ac1bec936ed8625d81bd937ac31d920
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-03-13 01:22:50 +0900

    section7_99

--- dist/generics.js
+++ dist/generics.js
@@ -1,5 +1,36 @@
 "use strict";
-function copy(value) {
+function copy(value, key) {
+    value[key];
     return value;
 }
-console.log(copy('hello'));
+console.log(copy({ name: 'Quill', age: 38 }, 'name'));
+class LinghtDatabase {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LinghtDatabase();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Grapre');
+console.log(stringLightDatabase.get());
+const fetchData = new Promise(resolve => {
+    setTimeout(() => {
+        resolve('hello');
+    }, 3000);
+});
+fetchData.then(data => {
+    data.toUpperCase();
+});
+const vegetables = ['Tomato', 'Broccoli'];
+let tmp2;
--- src/generics.ts
+++ src/generics.ts
@@ -1,4 +1,48 @@
-function copy<T,U>(value: T):T {
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
+    value[key];
     return value;
 }
-console.log(copy('hello'))
+console.log(copy({ name: 'Quill', age: 38 }, 'name'))
+
+class LinghtDatabase<T extends string | number | boolean> {
+    private data: T[] = [];
+    add(item: T) {
+        this.data.push(item);
+    }
+    remove(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LinghtDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Grapre')
+console.log(stringLightDatabase.get());
+
+interface Todo {
+    title: string;
+    text: string;
+}
+type Todoable = Partial<Todo>
+type ReadTodo = Readonly<Todo>
+
+const fetchData:Promise<string> = new Promise(resolve => {
+    setTimeout(() => {
+        resolve('hello')
+    }, 3000);
+})
+
+fetchData.then(data => {
+    data.toUpperCase();
+})
+const vegetables: Array<string> = ['Tomato', 'Broccoli']
+
+interface ResponseData<T extends{message: string}= any> {
+    data: T;
+    status: number;
+}
+let tmp2: ResponseData
