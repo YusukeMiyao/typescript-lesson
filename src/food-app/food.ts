098a24fd546d47cd8d65398ba15986e82decbdc5
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-03-16 17:30:50 +0900

    section10_138

---
+++ src/food-app/food.ts
@@ -0,0 +1,13 @@
+import { Score } from './score';
+import { Foodable } from './interfaces';
+
+export class Food implements Foodable {
+    constructor(public element: HTMLDivElement) {
+        element.addEventListener('click',this.clickEventHandler.bind(this))
+    }
+    clickEventHandler() {
+        this.element.classList.toggle('food--active')
+        const score = Score.getInstance();
+        score.render()
+    }
+ }
